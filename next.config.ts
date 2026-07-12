8ec3be285efeb5fac1cade31d055f536f600ee9c
Gayatri Samal <samalg2502@gmail.com>
2026-07-13 01:13:46 +0530

    did config changes

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,24 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig: NextConfig = {
   output: "export",
   trailingSlash: true,
+
   images: {
     unoptimized: true,
   },
+
   basePath: isProd ? "/portfolio" : "",
   assetPrefix: isProd ? "/portfolio/" : "",
+
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.(glb|gltf)$/,
+      use: {
+        loader: "file-loader",
+      },
+    });
+
+    return config;
+  },
 };
 
 export default nextConfig;
